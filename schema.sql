da1a100d07fc2098531544f0758351c9da94f9b8
GUDE VISHNU VARDHAN <gudevishnuvardhan@gmail.com>
2026-07-06 21:21:03 +0530

    Delete schema.sql

--- schema.sql
+++
@@ -1,8 +0,0 @@
- CREATE TABLE state_electricity_consumption (
-    state_name VARCHAR(100) NOT NULL,
-    region VARCHAR(50),
-    reading_date DATE NOT NULL,
-    usage_mwh NUMERIC(12, 2), 
-    latitude NUMERIC(9, 6),   
-    longitude NUMERIC(9, 6)   
-);
